2f77119315aa136974079beac3eaf1eeb578a617
Sean Lee <weilonge@gmail.com>
2015-08-21 03:50:35 +0800

    unit-test.

--- apps/system/test/unit/mock_places.js
+++ apps/system/test/unit/mock_places.js
@@ -4,6 +4,32 @@
 function MockPlaces() {}
 
 MockPlaces.prototype = {
+  init: function() {
+    this._store = {};
+  },
   start: function() {},
+
+  defaultPlace: function(url) {
+    return {
+      url: url,
+      title: url,
+      icons: {},
+      frecency: 0,
+      // An array containing previous visits to this url
+      visits: [],
+      screenshot: null
+    };
+  },
+
+  editPlace: function(url, fun) {
+    return new Promise((resolve, reject) => {
+      var place = this._store[url] || this.defaultPlace(url);
+      fun(place, (newPlace) => {
+        this._store[url] = place;
+        resolve();
+      });
+    });
+  },
+
   clear: function() {}
 };
